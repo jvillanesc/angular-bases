8ca94484c5b67f82c357e39a5603ebdc786881ed
jvillanes <jvillanesc>
2023-10-15 06:44:45 -0500

    End section 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
+import { HerosModule } from './heroes/heores.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HerosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
